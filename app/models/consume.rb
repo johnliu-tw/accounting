95192f23c8c24b58409035397260efdbe3d818f6
nick66551 <john831118@gmail.com>
2015-05-18 21:35:23 +0800

    Added rails_12factor gem and updated Gemfile.lock

--- app/models/consume.rb
+++ app/models/consume.rb
@@ -1,3 +1,7 @@
 class Consume < ActiveRecord::Base
 	belongs_to :account 
+	def self.dashboard
+		data = Consume.select("consume_id,sum(cost)as total").consume("cost")
+		data.map{|d|[Consume.find(d.cost).name,d.total]}
+	end
 end
